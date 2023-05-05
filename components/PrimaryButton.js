7da3b0806323c3783b51dd1e550ac845b6d5239e
Triankit <tripathi.ankit83@gmail.com>
2023-05-05 11:04:57 +0530

    first commit

---
+++ components/PrimaryButton.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {View, Pressable, Text, StyleSheet} from 'react-native';
+
+function PrimaryButton({children}) {
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({pressed}) =>
+          pressed
+            ? [styles.buttonInnerContainer, styles.pressed]
+            : styles.buttonInnerContainer
+        }
+        android_ripple={'#5e0230'}>
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    margin: 4,
+    borderRadius: 28,
+    overflow: 'hidden',
+  },
+  buttonInnerContainer: {
+    backgroundColor: '#72063c',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+
+    elevation: 2,
+    shadowColor: 'black',
+    shadowOffset: {width: 1, height: 2},
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
